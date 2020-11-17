d7b28faaac6fed245ff637ef121649afabae610c
Konrad Pilewski <pilewski.konrad1@gmail.com>
2020-11-17 22:10:21 +0100

    commit to ex6

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Pilewski, Andrzejewski sortowanie={orderBy}";
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Pilewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+        
+        //POST
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //TODO add to database & generate index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+    }
+}
