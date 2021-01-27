63dbc8fa6ebd99776a9e2ac64c9698fa2c353cc9
Konrad Pilewski <pilewski.konrad1@gmail.com>
2021-01-27 18:26:53 +0100

    powrót do poprzedniej wersji i małe fixy do zad 5

---
+++ cw3/cw3/DAL/SqlDbService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using cw3.Models;
+
+namespace cw3.DAL
+{
+    public class SqlDbService : IDbService
+    {
+        public IEnumerable<Student> GetStudents()
+        {
+            return null;
+        }
+    }
+}
